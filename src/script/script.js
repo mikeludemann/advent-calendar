cb3f8517830b520eeb739d498d0c83a776c0c406
Mike Ludemann <mike-ludemann@hotmail.de>
2019-07-25 12:14:46 +0200

    Update external json files - Create a script for loading the external json file - Setup default modal for all days

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,9 @@
+/**
+ * Month and Day validator
+ * 
+ * Modal with external data
+ */
+
 $(function(){
 
 	var today = new Date(),
@@ -6,26 +12,62 @@ $(function(){
 
 	$('.number').click(function(){
 
-    var selectDay = Number(this.innerText);
-    
-    if(month == 12){
+		var selectDay = Number(this.innerText);
 
-      if (day === selectDay || day > selectDay) {
+		if(month == 12){
 
-        console.log("Visible at: " + selectDay);
-  
-      } else {
-  
-        console.log("Not available");
-  
-      }
+			if (day === selectDay || day > selectDay) {
 
-    } else {
-  
-      console.log("December is not to be started at the moment.");
+				console.log("Visible at: " + selectDay);
+
+			} else {
+
+				console.log("Not available");
+
+			}
+
+		} else {
+
+			console.log("December is not to be started at the moment.");
+
+		}
+
+	});
+
+});
+
+// Load external JSON-File
+
+function loadJSON(url, callback) { 
+
+  var xmlHTTP = new XMLHttpRequest();
+
+  xmlHTTP.overrideMimeType("application/json");
+  xmlHTTP.open('GET', url, true);
+  xmlHTTP.onreadystatechange = function () {
+
+    if (xmlHTTP.readyState == 4 && xmlHTTP.status == "200") {
+
+      callback(xmlHTTP.responseText);
 
     }
 
-	});
+  };
+
+  xmlHTTP.send(null); 
+
+}
+
+// Get external JSON data
+
+$(document).ready(function(){
+
+  loadJSON("./src/script/data.json", function(response) {
+    
+    var data = JSON.parse(response);
+
+    console.log(data);
+      
+  });
 
 });
